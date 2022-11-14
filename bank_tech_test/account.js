721239e93b5698d89cf19b38f16e259803e8d931
Na-Rae Kim <narae1096@gmail.com>
2022-11-14 15:08:48 +0000

    balance after withdrawal

--- bank_tech_test/account.js
+++ bank_tech_test/account.js
@@ -1,9 +1,17 @@
+const { middleware } = require("yargs");
+
 class Account {
   constructor() {
     this.balance = 0;
+    this.transactions = [];
   }
   deposit(num) {
     this.balance += num;
+    return this.balance;
+  }
+
+  withdraw(num) {
+    this.balance -= num;
     return this.balance;
   }
 }
--- bank_tech_test/account.test.js
+++ bank_tech_test/account.test.js
@@ -6,9 +6,31 @@ describe("Account", () => {
     expect(account.balance).toBe(0.0);
   });
 
-  it("add the deposit to balance", () => {
+  it("adds the deposit to balance", () => {
     const account = new Account();
     account.deposit(1000);
     expect(account.balance).toBe(1000.0);
+  });
+
+  it("adds the deposits to balance", () => {
+    const account = new Account();
+    account.deposit(1000);
+    account.deposit(500);
+    expect(account.balance).toBe(1500);
+  });
+
+  it("calculates the balance after withdrawal", () => {
+    const account = new Account();
+    account.balance = 1000;
+    account.withdraw(500);
+    expect(account.balance).toBe(500.0);
+  });
+
+  it("calculates the balance after withdrawals", () => {
+    const account = new Account();
+    account.balance = 1000;
+    account.withdraw(500);
+    account.withdraw(120);
+    expect(account.balance).toBe(380);
   });
 });
